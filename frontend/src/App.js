16641b602a088598e5bd34cf03a6d84543bf3e4d
Adhithyaa1-tech <achenji02@gmail.com>
2023-05-04 16:38:49 +0530

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import Header from "./Components/Header/Header.js";
+
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <div className='app'>
+          <Header/>
+
+        </div>
+
+      </Router>
     </>
   );
 }
